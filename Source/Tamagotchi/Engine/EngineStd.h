d1194df92952fe34746fd2ce7cea894f5ecdae88
Arvydas Sidorenko <asido4@gmail.com>
2013-06-11 20:11:49 +0100

    Added usefull macros and important includes to pre-compiled header.

--- Source/Tamagotchi/Engine/EngineStd.h
+++ Source/Tamagotchi/Engine/EngineStd.h
@@ -8,10 +8,34 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+
+#include <algorithm>
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <map>
+
 #ifdef _WIN32
-#include <windows.h>
+#	if defined(_DEBUG)
+#		define T_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#	else
+#		define T_NEW new
+#	endif
+#else
+#	define T_NEW new
 #endif // _WIN32
 
-#include <stdlib.h>
+
+#if !defined(SAFE_DELETE)
+#	define SAFE_DELETE(x)	if (x) delete x; x = NULL;
+#endif
+
+#if !defined(SAFE_DELETE_ARRAY)
+#	define SAFE_DELETE_ARRAY(x)	if (x) delete[] x; x = NULL;
+#endif
 
 #endif // __ENGINESTD_H__
