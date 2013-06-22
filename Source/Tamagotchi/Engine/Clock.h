b28675324cb3b5aa4c80373b2ec385a202970d61
Arvydas Sidorenko <asido4@gmail.com>
2013-06-22 05:26:26 +0100

    Introduced Clock class.

---
+++ Source/Tamagotchi/Engine/Clock.h
@@ -0,0 +1,27 @@
+#ifndef __CLOCK_H__
+#define __CLOCK_H__
+
+#if defined(WIN32)
+#   include <windows.h>
+#endif
+
+class Clock
+{
+public:
+    static bool Init();
+
+    // Don't try to convert it to calendar time as Windows and POSIX return milliseconds
+    // based on different reference point. This function is suitable to calculate the
+    // elapsed milliseconds.
+    static long long GetTimeMilli();
+
+private:
+    static Clock& Get();
+
+private:
+#if defined(WIN32)
+    LARGE_INTEGER llFrequency;
+#endif
+};
+
+#endif // __CLOCK_H__
