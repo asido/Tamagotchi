86a1caf5120cf6b75a7b31d489cb812fcc2767a1
Arvydas Sidorenko <asido4@gmail.com>
2013-06-22 01:19:25 +0100

    Implemented mutex.

---
+++ Source/Tamagotchi/Engine/Threading/Mutex.cpp
@@ -0,0 +1,115 @@
+#include "Mutex.h"
+
+//-----------------------------------------------
+// Public
+//-----------------------------------------------
+
+Mutex::Mutex()
+{
+    Init();
+}
+
+Mutex::Mutex(const Mutex &m)
+{
+    Init();
+
+    if (m.locked && !this->locked)
+    {
+        Lock();
+    }
+    else if (!m.locked && locked)
+    {
+        Unlock();
+    }
+}
+
+Mutex::~Mutex()
+{
+#if defined(WIN32)
+    DeleteCriticalSection(&mutex);
+#elif defined(__APPLE__)
+    pthread_mutex_unlock(&mutex);
+    pthread_mutex_destroy(&mutex);
+#else
+#   error implement me
+#endif
+}
+
+Mutex& Mutex::operator=(const Mutex &m)
+{
+    if (m.locked && !locked)
+    {
+        Lock();
+    }
+    else if (!m.locked && locked)
+    {
+        Unlock();
+    }
+
+    return *this;
+}
+
+bool Mutex::Lock()
+{
+    locked = true;
+
+#if defined(WIN32)
+    EnterCriticalSection(&mutex);
+    return true;
+#elif defined(__APPLE__)
+    return pthread_mutex_lock(&mutex) == 0;
+#else
+#   error implement me
+#endif
+}
+
+bool Mutex::TryLock()
+{
+    locked = true;
+
+#if defined(WIN32)
+    return TryEnterCriticalSection(&mutex) == TRUE;
+#elif defined(__APPLE__)
+    return pthread_mutex_trylock(&mutex) == 0;
+#else
+#   error implement me
+#endif
+}
+
+bool Mutex::Unlock()
+{
+    locked = false;
+
+#if defined(WIN32)
+    LeaveCriticalSection(&mutex);
+    return true;
+#elif defined(__APPLE__)
+    return pthread_mutex_unlock(&mutex) == 0;
+#else
+#   error implement me
+#endif
+}
+
+bool Mutex::IsLocked() const
+{
+    return locked;
+}
+
+//-----------------------------------------------
+// Private
+//-----------------------------------------------
+
+void Mutex::Init()
+{
+#if defined(WIN32)
+    InitializeCriticalSection(&mutex);
+#elif defined(__APPLE__)
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&mutex, &attr);
+    pthread_mutexattr_destroy(&attr);
+#else
+#   error implement me
+#endif
+}
---
+++ Source/Tamagotchi/Engine/Threading/Mutex.h
@@ -0,0 +1,41 @@
+#ifndef __MUTEX_H__
+#define __MUTEX_H__
+
+#if defined(WIN32)
+#   include <windows.h>
+#elif defined(__APPLE__)
+#   include <pthread.h>
+#else
+#error implement me
+#endif
+
+class Mutex
+{
+public:
+    Mutex();
+    Mutex(const Mutex &m);
+    ~Mutex();
+
+    Mutex& operator=(const Mutex &m);
+
+    bool Lock();
+    bool TryLock();
+    bool Unlock();
+    bool IsLocked() const;
+
+private:
+    void Init();
+
+private:
+#if defined(WIN32)
+    CRITICAL_SECTION mutex;
+#elif defined(__APPLE__)
+    pthread_mutex_t mutex;
+#else
+#   error implement me
+#endif
+
+    bool locked;
+};
+
+#endif // __MUTEX_H__
