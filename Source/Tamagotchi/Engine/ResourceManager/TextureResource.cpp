c83e41c55f0f6ca016d388d5762da2de11cbcf86
Arvydas Sidorenko <asido4@gmail.com>
2013-07-01 16:48:57 +0300

    Use GL_LINEAR texture filter instead of GL_NEAREST.

--- Source/Tamagotchi/Engine/ResourceManager/TextureResource.cpp
+++ Source/Tamagotchi/Engine/ResourceManager/TextureResource.cpp
@@ -152,8 +152,8 @@ bool TextureResourceLoader::LoadResource(char *rawBuffer, unsigned int rawSize, 
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexImage2D(GL_TEXTURE_2D, 0, pngGlFormat, pngWidth, pngHeight, 0, pngGlFormat, GL_UNSIGNED_BYTE, decompressedBuffer);
